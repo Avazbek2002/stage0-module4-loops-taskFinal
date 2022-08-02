33ba26b13edb9e808434d6dc37693ad8b9f7e292
Avazbek Isroilov <64166521+Avazbek2002@users.noreply.github.com>
2022-08-02 17:14:52 +0500

    hello

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,10 +2,24 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 0; j < (cathetusLength - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j > 1; j--) {
+                System.out.print(j);
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            for (int j = 0; j < cathetusLength - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+        new Pyramid().printPyramid(3);
     }
 }
